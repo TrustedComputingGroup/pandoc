3b26bcccdfeff741bf4cf0b063c6f71923e9f18e
Chris Fenner <commits@dlp.rip>
2022-08-20 16:16:06 -0700

    add the informative text filter for docx

---
+++ filter/info.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+"""
+Pandoc filter to convert all block quotes to TCG Informative text.
+"""
+
+from pandocfilters import toJSONFilter, Str, Div, attributes
+
+def informative(key, value, format, meta):
+  if key == 'BlockQuote':
+    return Div(attributes({'custom-style': 'TCG Informative'}), value)
+
+if __name__ == "__main__":
+  toJSONFilter(informative)
