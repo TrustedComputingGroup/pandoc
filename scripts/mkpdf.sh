ca0c9902efaa9292951d432ac91316927e990168
Chris Fenner <commits@dlp.rip>
2023-12-03 10:44:25 -0800

    Fix up mkpdf.sh
    This change fixes mkpdf.sh to take the dependency on build.sh inside the
    Docker container instead of doing the fixups itself. It simplifies the
    script down to:
    
    1. Create and mount a temp directory for simplified file I/O to the
       Docker container
    2. Use docker run but with a customized entrypoint that also copies
       files to the current directory and back to the working directory
    3. Check for errors

--- scripts/mkpdf.sh
+++ scripts/mkpdf.sh
@@ -13,54 +13,19 @@ if [[ ! -f "$MD" ]]; then
 fi
 
 cp $MD $WD/spec.md
-MD=$WD/spec.md
 DOCKER_MD=$DOCKER_WD/spec.md
 
-# \newpage is rendered as the string "\newpage" in GitHub markdown.
-# Transform horizontal rules into \newpages.
-# Exception: the YAML front matter of the document, so undo the instance on the first line.
-sed -i 's/^---$/\\newpage/g;1s/\\newpage/---/g' $MD
+OUT="${DOCKER_WD}/output.pdf"
 
-# Transform sections before the table of contents into addsec, which does not number them.
-# While we're doing this, transform the case to all-caps.
-sed -i '0,/\\tableofcontents/s/^# \(.*\)/\\addsec\{\U\1\}/g' $MD
-
-# Get data
-DATE=$(grep "date:" $MD | head -n 1 | cut -d ' ' -f 2)
-YEAR=$(date --date=$DATE +%Y)
-DATE_ENGLISH=$(date --date=$DATE "+%B %-d, %Y")
-
-OUT="${DOCKER_WD}/tcgspec.pdf"
-
-# Make PDF
-PANDOC_OPTS="--embed-resources \
-  --standalone \
-  --template=/resources/template/eisvogel.latex \
-  --filter=mermaid-filter \
-  --filter=pandoc-crossref \
-  --resource-path=.:/resources \
-  --data-dir=/resources \
-  --top-level-division=section \
-  --variable=block-headings \
-  --variable=numbersections \
-  --variable=table-use-row-colors \
-  --metadata=date-english:\"${DATE_ENGLISH}\" \
-  --metadata=year:"${YEAR}" \
-  --metadata=titlepage:true \
-  --metadata=titlepage-background:/resources/img/greentop.png \
-  --metadata=logo:/resources/img/tcg.png \
-  --metadata=titlepage-rule-height:0 \
-  --metadata=colorlinks:true \
-  --metadata=contact:admin@trustedcomputinggroup.org \
-  --from=markdown+implicit_figures+table_captions \
-  --to=pdf \
-  --output=${OUT}"
-
+SCRIPT_OPTS="--pdf=output.pdf spec.md"
 DOCKER_OPTS="-v $WD:$DOCKER_WD"
+DOCKER_COMMAND="cp $DOCKER_MD .; /usr/bin/build.sh $SCRIPT_OPTS; cp ./output.* $DOCKER_WD"
 IMAGE_NAME="ghcr.io/trustedcomputinggroup/pandoc"
 
-# Quotes around DATE_ENGLISH get messed up during expansion. Instead,
-# quotes are added menually above and eval is used to execute the command.
-eval "docker run $DOCKER_OPTS $IMAGE_NAME $DOCKER_MD $PANDOC_OPTS"
+eval "docker run $DOCKER_OPTS --entrypoint /bin/sh $IMAGE_NAME -c '$DOCKER_COMMAND'"
 
-echo "Generated spec at $OUT"
+if [ $? -eq 0 ]; then
+  echo "Generated spec at $OUT"
+else
+  echo "Failed to generate spec"
+fi
