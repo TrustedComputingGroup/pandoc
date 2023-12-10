99d1298fa2bdd5da1ed18ef4caf0a5c7a17e811c
Chris Fenner <commits@dlp.rip>
2023-12-10 14:35:25 -0800

    add RawInline support to the lua filter

--- filter/parse-html.lua
+++ filter/parse-html.lua
@@ -6,3 +6,9 @@ function RawBlock (raw)
       or raw
   end
   
+  function RawInline (raw)
+    return raw.format:match 'html'
+      and pandoc.read(raw.text, 'html').blocks
+      or raw
+  end
+  
