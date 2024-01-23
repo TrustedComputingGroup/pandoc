a015d65649280220ae0d107064680f89f11af234
Chris Fenner <cfenn@google.com>
2024-01-23 23:11:42 +0000

    add a filter that can convert fenced divs to informative blocks

---
+++ filter/style-fenced-divs.lua
@@ -0,0 +1,57 @@
+-- style fenced divs according to the class
+
+-- This table lists all the supported classes and the color of the box for each.
+div_classes =
+{
+  ["informative"] = {
+    ["label"] = "Informative Comment:",
+    ["background"] = "informative-background",
+    ["foreground"] = "informative-foreground",
+  },
+  ["note"] = {
+    ["label"] = "Note:",
+    ["background"] = "informative-background",
+    ["foreground"] = "informative-foreground",
+  },
+  ["example"] = {
+    ["label"] = "Example:",
+    ["background"] = "informative-background",
+    ["foreground"] = "informative-foreground",
+  },
+  ["caveat"] = {
+    ["label"] = "Caveat:",
+    ["background"] = "orange-background",
+    ["foreground"] = "orange-foreground",
+  },
+  ["tip"] = {
+    ["label"] = "Tip:",
+    ["background"] = "green-background",
+    ["foreground"] = "green-foreground",
+  },
+  ["warning"] = {
+    ["label"] = "Warning:",
+    ["background"] = "red-background",
+    ["foreground"] = "red-foreground",
+  }
+}
+
+function Div(el)
+    local class = el.classes[1]
+    if(class) then
+      local class_spec = div_classes[string.lower(class)]
+      if(class_spec) then
+        local color_bg = class_spec["background"]
+        local color_fg = class_spec["foreground"]
+        local label = class_spec["label"]
+        return {
+          pandoc.RawBlock('latex', '\\vskip 0.5em'),
+          pandoc.RawBlock('latex', string.format('\\begin{mdframed}[linewidth=0pt,backgroundcolor=%s,skipabove=\\parskip,nobreak=true]', color_bg)),
+          pandoc.RawBlock('latex', string.format('\\textbf{\\textit{\\textcolor{%s}{\\small %s}}}', color_fg, label)),
+          pandoc.RawBlock('latex', string.format('\\color{%s}', color_fg)),
+          el,
+          pandoc.RawBlock('latex', '\\end{mdframed}')
+        }
+      end
+    end
+    return el
+  end
