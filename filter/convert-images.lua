891c7374265fa3b02bb1fba8447185e2ec64a6b4
Chris Fenner <cfenn@google.com>
2024-02-12 16:08:00 +0000

    suppress output during image generation

--- filter/convert-images.lua
+++ filter/convert-images.lua
@@ -1,21 +1,34 @@
 -- Convert a variety of image formats to PDF
 
+function runCommandSuppressOutput(command)
+    -- N.B.: we are using io.popen so we can suppress the output of the command.
+    local pipe = io.popen(command)
+    if not pipe then
+        return false
+    end
+    pipe:flush()
+    local output = pipe:read("*all")
+    pipe:close()
+    return true
+end
 
 -- Wrap calls to drawio in xvfb-run. Note that --no-sandbox has to be the last argument.
 -- https://github.com/jgraph/drawio-desktop/issues/249
 function drawio(source, dest)
-    if not os.execute(string.format("xvfb-run -a drawio -x -f pdf -o %s %s --no-sandbox", dest, source)) then
+    if not runCommandSuppressOutput(string.format("xvfb-run -a drawio -x -f pdf -o %s %s --no-sandbox 2>&1", dest, source)) then
         print(string.format('failed to convert %s to %s using drawio, falling back to letting latex try to pick it up', img.src, new_filename))
         return false
     end
+    print(string.format("  Converted %s to %s with drawio.", source, dest))
     return true
 end
 
 function imagemagick(source, dest)
-    if not os.execute(string.format("convert -density 300 %s %s", source, dest)) then
+    if not runCommandSuppressOutput(string.format("convert -density 300 %s %s 2>&1", source, dest)) then
         print(string.format('failed to convert %s to %s using imagemagick, falling back to letting latex try to pick it up', img.src, new_filename))
         return false
     end
+    print(string.format("  Converted %s to %s with ImageMagick.", source, dest))
     return true
 end
 
