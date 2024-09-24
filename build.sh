35fa81975adcac9aa6119017f971633a4610c020
Chris Fenner <cfenn@google.com>
2024-09-24 15:37:45 +0000

    Only check out right parent on PRs

--- build.sh
+++ build.sh
@@ -245,15 +245,18 @@ fi
 # figure out git version and revision if needed.
 EXTRA_PANDOC_OPTIONS=""
 if test "${DO_GITVERSION}" == "yes"; then
-	# If HEAD is a merge commit, go to the first parent.
-	if $(git rev-parse HEAD^2 >/dev/null 2>/dev/null); then
+	if [ ! -z "${PR_NUMBER}" ] && $(git rev-parse HEAD^2 >/dev/null 2>/dev/null); then
+		# For PR workflows, check out the right parent so that filenames etc.
+		# are based on the most recent commit on the source branch.
+		# In the context of a GitHub pull request, HEAD is a merge commit where
+		# parent1 (HEAD^1) is the target branch and parent2 (HEAD~2) is the source
 		parent1=$(git rev-parse HEAD^1 2>/dev/null)
 		parent2=$(git rev-parse HEAD^2 2>/dev/null)
 		echo "Merge commit detected:"
 		echo "1: $parent1"
 		echo "2: $parent2"
-		echo "Checking out $parent1."
-		git checkout $parent1
+		echo "Checking out $parent2."
+		git checkout $parent2
 	fi
 
 	# TODO: Should we fail if dirty?
