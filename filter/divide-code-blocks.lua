f99a3b9c2ba2e12f5034143c4aa7d58d931a0dcc
Jeff Andersen <jeffandersen@google.com>
2025-04-23 11:52:51 -0700

    Allow code blocks to customize their font size. (#224)

--- filter/divide-code-blocks.lua
+++ filter/divide-code-blocks.lua
@@ -1,12 +1,32 @@
 -- draw horizontal rules above and below code blocks to separate them nicely
 
+code_classes =
+{
+    ["normal"] = {
+        ["font"] = "\\small",
+    },
+    ["small"] = {
+        ["font"] = "\\scriptsize",
+    },
+    ["tiny"] = {
+        ["font"] = "\\tiny",
+    },
+}
+
 function CodeBlock(block)
+    local class = block.classes[1]
+    local class_spec = code_classes[string.lower(class or "")]
+    if not class_spec then
+        class_spec = code_classes["normal"]
+    end
+    font = class_spec["font"]
     return {
-        pandoc.RawInline('latex', [[
-            \vskip 3pt%
+        pandoc.RawInline('latex', string.format([[
+            \vskip 3pt
             \begin{customcodeblock}
             \textbf{\textit{\textcolor{codeblock-header}{\small \BeginDemarcated{Code}}}}
-        ]]),
+            %s
+        ]], font)),
         block,
         pandoc.RawInline('latex', [[
             \textbf{\textit{\textcolor{codeblock-header}{\small \EndDemarcated{Code}}}}
