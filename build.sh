TrustedComputingGroup/pandoc

commit 5a8f24a78292dab0f2118b535f3794cd2138cf20

Improve diagram rendering. (#232)
- Add support for inline or included plantuml diagrams.
- Use figure identifiers for rendered mermaid / plantuml / aavg
  diagrams, allowing deletion of stale renders.
- Collate all rendered output (including latex cache files) to a .cache/ directory.
- Fix permissions for cached/generated files.

build.sh
@@ -264,6 +264,11 @@ if [[ -d /extra_resources ]]; then
 fi
 cd "${BUILD_DIR}"
 
+readonly src_uidgid=$(stat -c "%u:%g" "${SOURCE_DIR}")
+
+# Directory to hold rendered images and other cache files.
+mkdir -p ".cache/"
+
 # Let git work
 git config --global --add safe.directory "${BUILD_DIR}"
 
@@ -711,20 +716,17 @@ analyze_latex_logs() {
 	fi
 }
 
-# Copy generated files (if any) back to the source directory so they can be cached and speed up future runs.
+# Copy file(s), assuming ownership of SOURCE_DIR.
+cp_chown() {
+	local src=$1
+	local dst=$2
+
+	rsync --archive --mkpath --chown="${src_uidgid}" "${src}" "${dst}"
+}
+
+# Sync generated files (if any) back to the source directory so they can be cached and speed up future runs.
 cache_generated_files() {
-	find . -type f \( \
-		-name "*.aux" -o \
-		-name "*.lof" -o \
-		-name "*.lot" -o \
-		-name "*.toc" -o \
-		-name "*.upa" -o \
-		-name "*.upb" -o \
-		-name "*.convert.pdf" -o \
-		-name "*.mermaid.pdf" -o \
-		-name "*.mermaid.svg" -o \
-		-name "*.aasvg.pdf" \
-	\) -exec cp --parents {} "${SOURCE_DIR}" \; 2>/dev/null
+	cp_chown .cache "${SOURCE_DIR}"
 }
 
 # Takes Markdown input and writes LaTeX output using pandoc.
@@ -744,11 +746,11 @@ do_latex() {
 		--standalone
 		--no-highlight
 		--template=${TEMPLATE_PDF}
-		--lua-filter=mermaid-filter.lua
-		--lua-filter=aasvg-filter.lua
-		--lua-filter=informative-sections.lua
+		--lua-filter=convert-diagrams.lua
+		--lua-filter=convert-aasvg.lua
 		--lua-filter=convert-images.lua
 		--lua-filter=center-images.lua
+		--lua-filter=informative-sections.lua
 		--lua-filter=parse-html.lua
 		--lua-filter=apply-classes-to-tables.lua
 		--lua-filter=landscape-pages.lua
@@ -786,6 +788,10 @@ do_latex() {
 	fi
 	local end=$(date +%s)
 	echo "Elapsed time: $(($end-$start)) seconds"
+
+	# Cache generated files now so they don't need to be
+	# re-rendered if the build later fails.
+	cache_generated_files
 }
 
 # Takes LaTeX input and writes PDF output and logs using the PDF engine of choice.
@@ -807,7 +813,7 @@ do_pdf() {
 	if [[ "${ignore_errors}" = "true" ]]; then
 		pdflatex="${pdflatex} -interaction=nonstopmode"
 	fi
-	latexmk "${input}" -pdflatex="${pdflatex}" -pdf -diagnostics > "${logfile}"
+	latexmk "${input}" -pdflatex="${pdflatex}" -pdf -diagnostics -output-directory=.cache/ > "${logfile}"
 	if [ $? -ne 0 ]; then
 		FAILED=true
 		echo "PDF output failed"
@@ -816,12 +822,11 @@ do_pdf() {
 	# Write any LaTeX errors to stderr.
 	>&2 grep -A 5 "] ! " "${logfile}"
 
-	cache_generated_files
 	echo "Elapsed time: $(($end-$start)) seconds"
 	# Write any LaTeX errors to stderr.
 	>&2 grep -A 5 "! " "${logfile}"
 	if [[ ! "${FAILED}" = "true" || "${ignore_errors}" = "true" ]]; then
-		mv "${temp_pdf_file}" "${output}"
+		cp_chown ".cache/${temp_pdf_file}" "${output}" && rm ".cache/${temp_pdf_file}"
 	fi
 	if [[ ! "${FAILED}" = "true" ]]; then
 		analyze_latex_logs "${logfile}"
@@ -852,8 +857,8 @@ do_docx() {
 	cmd=(pandoc
 		--embed-resources
 		--standalone
-		--lua-filter=mermaid-filter.lua
-		--lua-filter=aasvg-filter.lua
+		--lua-filter=convert-diagrams.lua
+		--lua-filter=convert-aasvg.lua
 		--lua-filter=convert-images.lua
 		--lua-filter=parse-html.lua
 		--lua-filter=apply-classes-to-tables.lua
@@ -876,8 +881,6 @@ do_docx() {
 	else
 		echo "DOCX output generated to file: ${output}"
 	fi
-
-	cache_generated_files
 }
 
 # Takes Markdown input and writes HTML output using pandoc.
@@ -897,8 +900,8 @@ do_html() {
 		--standalone
 		--template=${TEMPLATE_HTML}
 		${HTML_STYLESHEET_ARGS}
-		--lua-filter=mermaid-filter.lua
-		--lua-filter=aasvg-filter.lua
+		--lua-filter=convert-diagrams.lua
+		--lua-filter=convert-aasvg.lua
 		--lua-filter=parse-html.lua
 		--lua-filter=apply-classes-to-tables.lua
 		--lua-filter=landscape-pages.lua
@@ -932,7 +935,6 @@ do_html() {
 		FAILED=true
 		echo "HTML output failed"
 	else
-		cache_generated_files
 		echo "HTML output generated to file: ${output}"
 	fi
 }
@@ -945,7 +947,7 @@ if [ -n "${PDF_OUTPUT}" -o -n "${LATEX_OUTPUT}" -o -n "${DIFFPDF_OUTPUT}" -o -n 
 	do_latex "${BUILD_DIR}/${INPUT_FILE}" "${TEMP_TEX_FILE}" "${CROSSREF_TYPE}" "${EXTRA_PANDOC_OPTIONS}"
 fi
 if [ -n "${LATEX_OUTPUT}" ]; then
-	cp "${TEMP_TEX_FILE}" "${SOURCE_DIR}/${LATEX_OUTPUT}"
+	cp_chown "${TEMP_TEX_FILE}" "${SOURCE_DIR}/${LATEX_OUTPUT}"
 fi
 
 # Generate the PDF output
@@ -955,8 +957,7 @@ if [ -n "${PDF_OUTPUT}" ]; then
 
 	# Copy the logs, if requested.
 	if [ -n "${PDFLOG_OUTPUT}" ]; then
-		mkdir -p "$(dirname ${SOURCE_DIR}/${PDFLOG_OUTPUT})"
-		cp "${LATEX_LOG}" "${SOURCE_DIR}/${PDFLOG_OUTPUT}"
+		cp_chown "${LATEX_LOG}" "${SOURCE_DIR}/${PDFLOG_OUTPUT}"
 	fi
 fi
 
@@ -1000,8 +1001,7 @@ if [ -n "${DIFFPDF_OUTPUT}" -o -n "${DIFFTEX_OUTPUT}" ]; then
 		else
 			do_diff_tex_fixups "${TEMP_DIFF_TEX_FILE}"
 			if [ -n "${DIFFTEX_OUTPUT}" ]; then
-				mkdir -p "$(dirname ${SOURCE_DIR}/${DIFFTEX_OUTPUT})"
-				cp "${TEMP_DIFF_TEX_FILE}" "${SOURCE_DIR}/${DIFFTEX_OUTPUT}"
+				cp_chown "${TEMP_DIFF_TEX_FILE}" "${SOURCE_DIR}/${DIFFTEX_OUTPUT}"
 			fi
 		fi
 	fi
@@ -1028,6 +1028,9 @@ if [ "${PRE_DIFFING_FAILED}" == "true" ]; then
 	echo "Overall workflow failed"
 	exit 1
 fi
+
+# Do a final sync for any later generated files.
+cache_generated_files
 
 echo "Overall workflow succeeded"
 exit 0

.gitignore
@@ -1,12 +1,4 @@
 *.pdf
 *.docx
 *.html
-/*.tex
-*.log
-*.aux
-*.lof
-*.lot
-*.toc
-*.fdb_latexmk
-*.upa
-*.upb
+.cache/*

Dockerfile
@@ -102,11 +102,14 @@ RUN apt update && apt install -y fontconfig && \
 
 RUN apt install -y \
     bash \
+    default-jre \
     moreutils \
     nodejs \
     npm \
+    rsync \
     sed \
-    software-properties-common
+    software-properties-common \
+    wget
 
 # Install Chromium via custom repo
 # https://askubuntu.com/questions/1204571/how-to-install-chromium-without-snap/1511695#1511695
@@ -118,6 +121,8 @@ ENV PUPPETEER_SKIP_CHROMIUM_DOWNLOAD=true \
     PUPPETEER_EXECUTABLE_PATH=/usr/bin/chromium-browser
 
 RUN npm install --global --unsafe-perm puppeteer@23.2.1 imgur@2.4.2 @mermaid-js/mermaid-cli@11.1.1 typescript@5.5.4 pandiff@0.6.0
+
+RUN wget -O /usr/share/plantuml.jar https://github.com/plantuml/plantuml/releases/download/v1.2025.2/plantuml-asl-1.2025.2.jar
 
 # Important: /usr/local/texlive/bin/ paths come before other paths. We want to use the texlive we
 # built above, not any that happen to have come along with our base image.
@@ -205,7 +210,6 @@ RUN apt install -y \
     libsecret-1-0 \
     libxss1 \
     openbox \
-    wget \
     xorg \
     xvfb
 

filter/convert-aasvg.lua
@@ -1,61 +1,40 @@
 -- Turn aasvg-classed code blocks into figures, retaining other classes on the
 -- code block as classes on the figure.
 
-function runCommandWithInput(command, input)
-    local pipe = io.popen(command, "w")
-    if not pipe then
-        return false
-    end
-    pipe:write(input)
-    pipe:flush()
-    pipe:close()
-    return true
-end
-
-function runCommandSuppressOutput(command)
-    -- N.B.: we are using io.popen so we can suppress the output of the command.
-    local pipe = io.popen(command)
-    if not pipe then
-        return false
-    end
-    pipe:flush()
-    local output = pipe:read("*all")
-    pipe:close()
-    return true
-end
-
-function getContentsHash(contents)
-    return pandoc.sha1(contents):sub(1,10)
-end
-
-function fileExists(file)
-    local f = io.open(file)
-    if f then
-        f:close()
-        return true
-    end
-    return false
-end
+-- Patch the path to include the current script's directory.
+package.path = package.path .. ";" .. debug.getinfo(1).source:match("@?(.*/)") .. "?.lua"
+utils = require "utils"
 
 function aasvgFigure(code, caption, attrs)
-    local filename_base = getContentsHash('code=' .. code .. 'caption=' .. pandoc.utils.stringify(caption) .. 'attrs=' .. pandoc.utils.stringify(attrs)) .. '.aasvg'
+    local filename_base = '.cache/' .. attrs.identifier .. '.' .. utils.getContentsHash('code=' .. code .. 'caption=' .. pandoc.utils.stringify(caption) .. 'attrs=' .. pandoc.utils.stringify(attrs)) .. '.aasvg'
     local filename_svg = filename_base .. '.svg'
     local filename_pdf = filename_base .. '.pdf'
 
-    if fileExists(filename_pdf) then
-        print(string.format('%s already exists; not re-rendering it', filename_pdf))
+    if utils.fileExists(filename_pdf) then
+        print(string.format("    not converting %s (already up-to-date as %s)", attrs.identifier, filename_pdf))
     else
         print(string.format('rendering %s using aasvg ...', filename_svg))
-        if not runCommandWithInput(string.format(
+        if not utils.runCommandWithInput(string.format(
             "aasvg --fill > %s 2>&1", filename_svg), code) then
             print(string.format('failed to convert ASCII art SVG (aasvg) diagram to %s using aasvg, falling back to letting LaTeX try to pick it up', filename_svg))
             return false
+        else
+            print(string.format("    rendered %s to %s", attrs.identifier, filename_svg))
         end
 
         print(string.format('converting %s to %s using rsvg-convert ...', filename_svg, filename_pdf))
-        if not runCommandSuppressOutput(string.format("rsvg-convert --format=pdf --keep-aspect-ratio --output %s %s 2>&1", filename_pdf, filename_svg)) then
+        if not utils.runCommandSuppressOutput(string.format("rsvg-convert --format=pdf --keep-aspect-ratio --output %s %s 2>&1", filename_pdf, filename_svg)) then
             print(string.format('failed to convert %s to %s using rsvg-convert, falling back to letting LaTeX try to pick it up', filename_svg, filename_pdf))
             return false
+        else
+            print(string.format('    converted %s to %s', filename_svg, filename_pdf))
+        end
+
+        -- Delete stale copies of the files. This makes it easier to cache only the latest converted pdfs.
+        -- Don't do this if the "keepstaleimages" variable is set.
+        if not PANDOC_WRITER_OPTIONS.variables["keepstaleimages"] then
+            utils.deleteFilesExcept('.cache/' .. attrs.identifier .. '*.aasvg.svg*', filename_svg)
+            utils.deleteFilesExcept('.cache/' .. attrs.identifier .. '*.aasvg.pdf*', filename_pdf)
         end
     end
 

filter/convert-diagrams.lua
@@ -1,0 +1,108 @@
+-- Turn mermaid and plantuml code blocks into figures, retaining other classes
+-- on the code block as classes on the figure.
+
+-- Patch the path to include the current script's directory.
+package.path = package.path .. ";" .. debug.getinfo(1).source:match("@?(.*/)") .. "?.lua"
+utils = require "utils"
+
+image_attrs =
+{
+    ["pdf"] = {},
+    ["html"] = {
+        "", -- No identifier
+        {}, -- No classes
+        {width = "500px"}
+    },
+}
+
+mermaid_extension =
+{
+    ["pdf"] = ".mermaid.pdf",
+    ["html"] = ".mermaid.svg",
+}
+
+-- Recursively resolve all UML includes ourselves, instead of letting plantuml do it.
+-- This ensures that cache files become invalidated if any included file changes.
+-- Doesn't try to detect infinite loops.
+function resolveUmlIncludes(uml_src_path, uml_content)
+    local src_dirname = uml_src_path:match("(.*/)")
+
+    -- A hack to make the regex below work, since gsub can't match `^ | \n`
+    if uml_content:match("^!include ") then
+        uml_content = "\n" .. uml_content
+    end
+
+    return uml_content:gsub("\n(!include )(%C+)", function(term, include_path)
+        -- Don't correct absolute paths.
+        if include_path:sub(1, 1) ~= "/" then
+            include_path = src_dirname .. include_path
+        end
+
+        print("    resolving UML include: " .. include_path)
+
+        included_content = utils.readFile(include_path)
+
+        return resolveUmlIncludes(include_path, included_content)
+    end)
+end
+
+-- `convert_command` must contain a single '%s', which will be replaced with the
+-- path to the output file.
+function convertFigure(code, caption, attrs, extension, convert_command)
+    local filename = '.cache/' .. attrs.identifier .. '.' .. utils.getContentsHash('code=' .. code .. 'caption=' .. pandoc.utils.stringify(caption) .. 'attrs=' .. pandoc.utils.stringify(attrs)) .. extension
+    if utils.fileExists(filename) then
+        print(string.format("    not converting %s (already up-to-date as %s)", attrs.identifier, filename))
+    else
+        print(string.format('rendering %s...', filename))
+        if not utils.runCommandWithInput(string.format(convert_command, filename), code) then
+            print(string.format('failed to convert diagram to %s, falling back to letting LaTeX try to pick it up', filename))
+            return false
+        else
+            print(string.format('    rendered %s to %s', attrs.identifier, filename))
+        end
+        -- Delete stale copies of this file. This makes it easier to cache only the latest converted diagrams.
+        -- Don't do this if the "keepstaleimages" variable is set.
+        if not PANDOC_WRITER_OPTIONS.variables["keepstaleimages"] then
+            utils.deleteFilesExcept('.cache/' .. attrs.identifier .. '*' .. extension .. '*', filename)
+        end
+    end
+
+    local img = pandoc.Image(caption, filename, "", image_attrs[FORMAT] or image_attrs["pdf"])
+    return pandoc.Figure(img, caption, attrs)
+end
+
+function mermaidFigure(code, caption, attrs)
+    return convertFigure(code, caption, attrs, mermaid_extension[FORMAT] or mermaid_extension["pdf"],
+        "mmdc --configFile /resources/filters/mermaid-config.json --puppeteerConfigFile ./.puppeteer.json --width 2000 --height 2000 --backgroundColor transparent --pdfFit --input - --output %s 2>&1")
+end
+
+function plantUmlFigure(code, caption, attrs)
+    code = resolveUmlIncludes("./", code)
+    return convertFigure(code, caption, attrs, ".plantuml.svg", "java -jar /usr/share/plantuml.jar -tsvg -pipe 2>&1 > %s")
+end
+
+function CodeBlock(el)
+    local diagramType = nil
+
+    local figure_classes = pandoc.List({})
+    for i, class in ipairs(el.classes) do
+        if class == 'mermaid' or class == 'plantuml' then
+            diagramType = class
+        else
+            figure_classes:insert(class)
+        end
+    end
+
+    if diagramType ~= nil then
+        local caption = {long = pandoc.Plain(pandoc.Str(el.attributes.caption))}
+        local attrs = pandoc.Attr(el.identifier, figure_classes)
+
+        if diagramType == 'mermaid' then
+            return mermaidFigure(el.text, caption, attrs)
+        end
+        if diagramType == 'plantuml' then
+            return plantUmlFigure(el.text, caption, attrs)
+        end
+    end
+    return el
+end

filter/convert-images.lua
@@ -1,49 +1,23 @@
 -- Convert a variety of image formats to PDF
 
-function runCommandSuppressOutput(command)
-    -- N.B.: we are using io.popen so we can suppress the output of the command.
-    local pipe = io.popen(command)
-    if not pipe then
-        return false
-    end
-    pipe:flush()
-    local output = pipe:read("*all")
-    pipe:close()
-    return true
-end
+-- Patch the path to include the current script's directory.
+package.path = package.path .. ";" .. debug.getinfo(1).source:match("@?(.*/)") .. "?.lua"
+utils = require "utils"
 
-function getFileHash(file)
-    local f = assert(io.open(file, "r"))
-    local contents = f:read("*all")
-    f:close()
-    return pandoc.sha1(contents):sub(1,10)
-end
-
-function fileExists(file)
-    local f = io.open(file)
-    if f then
-        f:close()
-        return true
+-- Only prepend '.cache/' if the path is not already in that directory.
+function ensurePathInCache(path)
+    if path:find('^[.]cache/') == nil then
+        return '.cache/' .. path
+    else
+        return path
     end
-    return false
-end
-
-function deleteFilesExcept(pattern, keep)
-    local f = io.popen(string.format("ls %s", pattern))
-    for filename in f:lines() do
-        if filename ~= keep then
-            os.remove(filename)
-            print(string.format("        deleted stale file %s", filename))
-        end
-    end
-    f:close()
 end
 
 -- Wrap calls to drawio in xvfb-run. Note that --no-sandbox has to be the last argument.
 -- https://github.com/jgraph/drawio-desktop/issues/249
 function drawio(source, dest)
     print(string.format('converting %s using drawio ...', source))
-    if not runCommandSuppressOutput(string.format("xvfb-run -a drawio -x -f pdf --crop -o %s %s --no-sandbox 2>&1", dest, source)) then
+    if not utils.runCommandSuppressOutput(string.format("xvfb-run -a drawio -x -f pdf --crop -o %s %s --no-sandbox 2>&1", dest, source)) then
         print(string.format('failed to convert %s to %s using drawio, falling back to letting LaTeX try to pick it up', source, dest))
         return false
     end
@@ -52,7 +26,7 @@ end
 
 function imagemagick(source, dest)
     print(string.format('converting %s using ImageMagick ...', source))
-    if not runCommandSuppressOutput(string.format("convert -density 300 %s %s 2>&1", source, dest)) then
+    if not utils.runCommandSuppressOutput(string.format("convert -density 300 %s %s 2>&1", source, dest)) then
         print(string.format('failed to convert %s to %s using ImageMagick, falling back to letting LaTeX try to pick it up', source, dest))
         return false
     end
@@ -61,7 +35,7 @@ end
 
 function svg(source, dest)
     print(string.format('converting %s using rsvg-convert...', source))
-    if not runCommandSuppressOutput(string.format("rsvg-convert --format=pdf --keep-aspect-ratio --output %s %s 2>&1", dest, source)) then
+    if not utils.runCommandSuppressOutput(string.format("rsvg-convert --format=pdf --keep-aspect-ratio --output %s %s 2>&1", dest, source)) then
         print(string.format('failed to convert %s to %s using rsvg-convert, falling back to letting LaTeX try to pick it up', source, dest))
         return false
     end
@@ -89,16 +63,18 @@ function Image (img)
     -- as well as speed up the LaTeX render iterations.
     local converter = converterFor(img.src)
     if converter then
-        local new_filename = img.src .. '.' .. getFileHash(img.src) .. '.convert.pdf'
-        if fileExists(new_filename) then
+        local new_filename = ensurePathInCache(img.src) .. '.' .. utils.getFileHash(img.src) .. '.convert.pdf'
+        utils.ensureDirExists(new_filename)
+
+        if utils.fileExists(new_filename) then
             print(string.format("    not converting %s (already up-to-date as %s)", img.src, new_filename))
             img.src = new_filename
         elseif converter(img.src, new_filename) then
             print(string.format("    converted %s to %s", img.src, new_filename))
-            -- Delete stale copies of this file. This makes it easier to cache only the latest converted pdfs
+            -- Delete stale copies of this file. This makes it easier to cache only the latest converted pdfs.
             -- Don't do this if the "keepstaleimages" variable is set.
             if not PANDOC_WRITER_OPTIONS.variables["keepstaleimages"] then
-                deleteFilesExcept(img.src .. "*.convert.pdf", new_filename)
+                utils.deleteFilesExcept(ensurePathInCache(img.src) .. "*.convert.pdf", new_filename)
             end
             img.src = new_filename
         end

filter/mermaid-filter.lua
@@ -1,81 +1,0 @@
--- Turn mermaid-classed code blocks into figures, retaining other classes on the
--- code block as classes on the figure.
-
-output_classes =
-{
-    ["pdf"] = {
-        ["extension"] = ".mermaid.pdf",
-        ["image_attrs"] = {},
-    },
-    ["html"] = {
-        ["extension"] = ".mermaid.svg",
-        ["image_attrs"] = {
-            "", -- No identifier
-            {}, -- No classes
-            {width = "500px"}
-        },
-    }
-}
-
-function runCommandWithInput(command, input)
-    local pipe = io.popen(command, "w")
-    if not pipe then
-        return false
-    end
-    pipe:write(input)
-    pipe:flush()
-    pipe:close()
-    return true
-end
-
-function getContentsHash(contents)
-    return pandoc.sha1(contents):sub(1,10)
-end
-
-function fileExists(file)
-    local f = io.open(file)
-    if f then
-        f:close()
-        return true
-    end
-    return false
-end
-
-function mermaidFigure(code, caption, attrs)
-    output_class = output_classes[FORMAT] or output_classes["pdf"]
-
-    local filename = getContentsHash('code=' .. code .. 'caption=' .. pandoc.utils.stringify(caption) .. 'attrs=' .. pandoc.utils.stringify(attrs)) .. output_class["extension"]
-    if fileExists(filename) then
-        print(string.format('%s already exists; not re-rendering it', filename))
-    else
-        print(string.format('rendering %s using Mermaid ...', filename))
-        if not runCommandWithInput(string.format(
-            "mmdc --configFile /resources/filters/mermaid-config.json --puppeteerConfigFile ./.puppeteer.json --width 2000 --height 2000 --backgroundColor transparent --pdfFit --input - --output %s 2>&1", filename), code) then
-            print(string.format('failed to convert Mermaid diagram to %s using mmdc, falling back to letting LaTeX try to pick it up', filename))
-            return false
-        end
-    end
-
-    local img = pandoc.Image(caption, filename, "", output_class["image_attrs"])
-    return pandoc.Figure(img, caption, attrs)
-end
-
-function CodeBlock(el)
-    local isMermaid = false
-    local figure_classes = pandoc.List({})
-    for i, class in ipairs(el.classes) do
-        if class == 'mermaid' then
-            isMermaid = true
-        else
-            figure_classes:insert(class)
-        end
-    end
-    if isMermaid then
-        local caption = {long = pandoc.Plain(pandoc.Str(el.attributes.caption))}
-        local attrs = pandoc.Attr(el.identifier, figure_classes)
-        el.identifier = nil
-        el.classes = {'mermaid'}
-        return mermaidFigure(el.text, caption, attrs)
-    end
-    return el
-end

filter/utils.lua
@@ -1,0 +1,71 @@
+-- A collection of helper functions.
+
+local utils = {}
+
+function utils.runCommandWithInput(command, input)
+    local pipe = io.popen(command, "w")
+    if not pipe then
+        return false
+    end
+    pipe:write(input)
+    pipe:flush()
+    pipe:close()
+    return true
+end
+
+function utils.runCommandSuppressOutput(command)
+    -- N.B.: we are using io.popen so we can suppress the output of the command.
+    local pipe = io.popen(command)
+    if not pipe then
+        return false
+    end
+    pipe:flush()
+    local output = pipe:read("*all")
+    pipe:close()
+    return true
+end
+
+function utils.readFile(path)
+    local f = assert(io.open(path, "rb"))
+    local content = f:read("*all")
+    f:close()
+    return content
+end
+
+function utils.getContentsHash(contents)
+    return pandoc.sha1(contents):sub(1,10)
+end
+
+function utils.getFileHash(file)
+    local f = assert(io.open(file, "r"))
+    local contents = f:read("*all")
+    f:close()
+    return pandoc.sha1(contents):sub(1,10)
+end
+
+function utils.fileExists(file)
+    local f = io.open(file)
+    if f then
+        f:close()
+        return true
+    end
+    return false
+end
+
+function utils.ensureDirExists(path)
+    local dirname = path:match("(.*/)")
+    pandoc.system.make_directory(dirname, true)
+end
+
+function utils.deleteFilesExcept(pattern, keep)
+    local f = io.popen(string.format("ls %s", pattern))
+    for filename in f:lines() do
+        if filename ~= keep then
+            os.remove(filename)
+            print(string.format("        deleted stale file %s", filename))
+        end
+    end
+    f:close()
+end
+
+return utils

commit fc9f78e966d1aef128b05dea69c150a561d9872b

Fix rsync command to prune stale cache images.
When a figure is re-rendered, stale versions of the figure are removed
from the cache. This change fixes that behavior, which was broken in
5a8f24a.

build.sh
@@ -741,7 +741,7 @@ cp_chown() {
 	local src=$1
 	local dst=$2
 
-	rsync --archive --mkpath --chown="${src_uidgid}" "${src}" "${dst}"
+	rsync --archive --mkpath --delete --chown="${src_uidgid}" "${src}" "${dst}"
 }
 
 # Sync generated files (if any) back to the source directory so they can be cached and speed up future runs.